ebb15712e6fabdf7cd33c677fdbb71b6aa195ba6
Akhil Manthina <akhilmanthina@gmail.com>
2024-08-25 18:02:36 -0700

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+import flowbite from "flowbite-react/tailwind";
+
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}", 
+    flowbite.content()
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter', 'sans-serif'],
+      },
+    },
+  },
+  plugins: [flowbite.plugin()],
+};
